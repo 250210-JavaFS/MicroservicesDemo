d2427c587db604b16edbb533e1eaa1509357409f
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-03-11 13:32:16 -0400

    get all books, no MSA stuff yet

--- BookAPI/src/main/java/com/revature/BookAPI/BookApiApplication.java
+++ BookAPI/src/main/java/com/revature/BookAPI/BookApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//No need for @ComponentScan etc, since our Classes are at the package level as main
 public class BookApiApplication {
 
 	public static void main(String[] args) {
---
+++ BookAPI/src/main/java/com/revature/BookAPI/controllers/BookController.java
@@ -0,0 +1,25 @@
+package com.revature.BookAPI.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Combination of @Controller and @ResponseBody
+@RequestMapping("/books")
+@CrossOrigin
+public class BookController {
+
+    //Method that doesn't interact with any other API
+    @GetMapping
+    public ResponseEntity<String[]> getAllBooks() {
+
+        String[] books = {"Flowers for Algernon", "Sherlock Holmes",
+                "The Taming of the Shrew", "The Shining", "The Very Hungry Caterpillar"};
+
+        return ResponseEntity.ok(books);
+
+    }
+
+}

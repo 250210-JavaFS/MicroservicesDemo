168c50f579fbe73228295128dffd1ec3e07b394d
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-03-14 13:35:22 -0400

    dependencies and build tags for lambdas

--- RecommendationAPI/src/main/java/com/revature/RecommendationAPI/RecommendationApiApplication.java
+++ RecommendationAPI/src/main/java/com/revature/RecommendationAPI/RecommendationApiApplication.java
@@ -2,6 +2,10 @@ package com.revature.RecommendationAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class RecommendationApiApplication {
@@ -9,5 +13,21 @@ public class RecommendationApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecommendationApiApplication.class, args);
 	}
+
+
+	/* Our entire functionality will live here now -
+	But I'll leave the controllers just for reference
+
+	Since Lambdas typically deploy APIs that have a single function, this is a typical pattern
+
+	Get Book Recs is going to be a Supplier function
+	Suppliers "Supply" data, and don't expect any data back*/
+
+	@Bean
+	public Supplier<List<String>> getBookRecs(){
+		return () -> List.of("Rec1", "Rec2", "Rec3");
+		//Yes, in a real app, this would probably just be a call to the service which calls a DAO
+	}
+
 
 }
